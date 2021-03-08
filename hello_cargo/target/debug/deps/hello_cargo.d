a260351d28ee774bac6fcbdc7424796fce5afa7e
ben1009 <liuhe1009@gmail.com>
2021-03-08 18:10:33 +0800

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/liuhe/proj/rustplayground/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/liuhe/proj/rustplayground/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/liuhe/proj/rustplayground/hello_cargo/target/debug/hello_cargo: /Users/liuhe/proj/rustplayground/hello_cargo/src/main.rs
