db6c6dfa86705c52be7fcc6ce85b6061b597f7cd
ben1009 <liuhe1009@gmail.com>
2023-02-08 11:38:48 +0800

    figureout vec item ownership

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fs, process};
+use std::{fs, mem, process};
 
 #[derive(Debug)]
 pub struct Config {
@@ -26,14 +26,19 @@ impl Config {
     //     }
     // }
 
-    pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {
+    pub fn new(args: &mut Vec<String>) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("args len < 3");
         }
 
-        let query = String::from(&args[1]);
+        let query = mem::take(&mut args[1]);
+        // https://stackoverflow.com/questions/27904864/what-does-cannot-move-out-of-index-of-mean
+        // Implicitly moving out of a Vec is not allowed as it would leave it in an invalid state
+        // — one element is moved out, the others are not.
+        // If you have a mutable Vec, you can use a method like Vec::remove to take a single value out:
         let file_path = args[2].to_string();
         let config = Config { file_path, query };
+
         Ok(config)
     }
 }
@@ -61,8 +66,8 @@ mod tests {
 
     #[test]
     fn args_len_less_3() {
-        let args = vec!["1".to_string(), "2".to_string()];
-        let ret = Config::new(args.as_slice());
+        let mut args = vec!["1".to_string(), "2".to_string()];
+        let ret = Config::new(&mut args);
         assert_eq!(ret.as_ref().unwrap().file_path, "");
         assert_eq!(ret.as_ref().unwrap().query, "2");
     }
