059cb5b6aa1d0519466c96b8a2cdb4df46434ad6
ben1009 <liuhe1009@gmail.com>
2023-03-08 16:44:01 +0800

    2. Add Two Numbers, really complicated using rust

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,27 +1,66 @@
-use std::collections::HashMap;
+fn main() {}
 
 struct Solution {}
 
-impl Solution {
-    #[allow(dead_code)]
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
-        for (i, n) in nums.iter().enumerate() {
-            map.insert(*n, i);
-        }
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
 
-        for (i, n) in nums.iter().enumerate() {
-            if let Some(v) = map.get_key_value(&(target - n)) {
-                if *v.1 == i {
-                    continue;
-                }
-
-                return vec![*v.1 as i32, i as i32];
-            }
-        }
-
-        vec![]
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        Self { next: None, val }
     }
 }
 
-fn main() {}
+impl Solution {
+    #[allow(dead_code)]
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let (mut l1, mut l2) = (l1, l2);
+        if l1.is_none() {
+            return l2;
+        }
+        if l2.is_none() {
+            return l1;
+        }
+
+        let mut ret = Some(Box::new(ListNode::new(-1)));
+        let mut head = &mut ret;
+        let mut carry = 0;
+        loop {
+            if l1.is_none() && l2.is_none() && carry == 0 {
+                break;
+            }
+            let v1 = match l1 {
+                Some(n1) => {
+                    l1 = n1.next;
+                    n1.val
+                }
+                None => 0,
+            };
+            let v2 = match l2 {
+                Some(n2) => {
+                    l2 = n2.next;
+                    n2.val
+                }
+                None => 0,
+            };
+            let mut num = v1 + v2 + carry;
+            carry = num / 10;
+            num %= 10;
+
+            let next = ListNode::new(0);
+            head.as_mut().unwrap().next = Some(Box::new(next));
+            head = &mut head.as_mut().unwrap().next;
+            head.as_mut().unwrap().val = num;
+        }
+
+        ret.unwrap().next
+    }
+}
